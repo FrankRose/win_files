54e1199a34cc89ba1f57bd566aa7bd80d92cf43a
FrankRose <37514407+FrankRose@users.noreply.github.com>
2021-06-15 14:32:57 -0400

    Add files via upload

---
+++ openme.bat
@@ -0,0 +1,2 @@
+echo Awesome. Your lab is operational. If you did this as part of your homework - great job! Remember this file for tomorrow as you will need to use the below code during the in-class lab. > "C:\Users\Jenny McCyber\Desktop\openme.txt"
+echo FS{Windows_is_now_op3ration@l} >> "C:\Users\Administrator\Desktop\openme.txt"
